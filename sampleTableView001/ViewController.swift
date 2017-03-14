ce8fc7b526ea4ccf8e09b9a0577eeb6e19a441ae
ei17ringo <ei17ringo@gmail.com>
2017-03-14 15:19:49 +0800

    commit init

--- sampleTableView001/ViewController.swift
+++ sampleTableView001/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// ①プロトコルの設定 UITableViewDelegate,UITableViewDataSource
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ④TableViewに指示をだすのがViewControllerだと設定する
+        // Storyboardで設定
+    }
+    
+    // ②行数を決定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // ③リストに表示する文字を決定し、表示
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 文字を表示するセルの取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // 表示文字の設定
+        //TODO:配列を使用して、プログラミング言語のリストを表示しましょう
+        cell.textLabel?.text = "\(indexPath.row)行目"
+        
+        // 文字を設定したセルを返す
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
